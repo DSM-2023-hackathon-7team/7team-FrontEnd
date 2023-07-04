998ac0dd755d0985e64ef06324d2568ab6a78410
dutexion <dutexion@gmail.com>
2023-07-04 20:22:52 +0900

    route

---
+++ src/pages/bulletin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Bulletin() {
+    return (
+        <>
+        hello
+        </>
+    )
+}
+
+export default Bulletin;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,8 @@ import Create from "../pages/create";
 import Quiz from "../pages/quiz";
 import Rank from "../pages/rank";
 import Quizgame from "../pages/quizgame";
+import Bulletin from "../pages/bulletin";
+import News from "../pages/news";
 
 export const Router = () => {
   return (
@@ -15,6 +17,8 @@ export const Router = () => {
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/rank" element={<Rank />} />
         <Route path="/quizgame" element={<Quizgame />} />
+        <Route path="/bulletin" element={<Bulletin />} />
+        <Route path="/news" element={<News />} />
       </Routes>
     </BrowserRouter>
   );
