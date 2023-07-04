e1f2516c0b67ad7d1410e7a5fa65aaad286d6787
dutexion <dutexion@gmail.com>
2023-07-04 18:59:35 +0900

    rank page

--- src/pages/rank.js
+++ src/pages/rank.js
@@ -1,10 +1,103 @@
 import React from "react";
+import { styled } from "styled-components";
+import Header from "../components/common/Header";
+
+const Body = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+`;
+
+const Img = styled.img`
+width: 250;
+height: 250px;
+margin-top:100px;
+`;
+
+const MedalImg = styled.img`
+width:120px;
+height:120px;
+margin-top:30px;
+`
+
+const Text1 = styled.div`
+margin-top:46px;
+color: #000;
+font-size: 32px;
+font-family: Inter;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`;
+
+const Text2 = styled.div`
+margin-top:9px;
+color: #000;
+font-size: 32px;
+font-family: Inter;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`;
+
+const ContainerBox = styled.div`
+margin-top:47px;
+display:flex;
+`;
+
+const Container = styled.div`
+width: 640px;
+height: 300px;
+border: 1px solid rgba(0, 0, 0, 0.20);
+display:flex;
+flex-direction:column;
+align-items:center;
+`;
+
+const RankText = styled.div`
+color: #000;
+font-size: 40px;
+font-family: Inter;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`;
+
+const ScoreText = styled.div`
+color: #000;
+font-size: 48px;
+font-family: Inter;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+margin-top:17px;
+`;
 
 function Rank() {
     return (
-        <>
-            test
-        </>
+        <Body>
+            <Header/>
+            <Img src="/images/trophy.svg"></Img>
+            <Text1>...님의 순위는 17등입니다!</Text1>
+            <Text2>...님의 점수는 280점입니다!</Text2>
+            <ContainerBox>
+            <Container>
+                <MedalImg src="/images/1medal.svg"/>
+                <RankText>1위</RankText>
+                <ScoreText>홍길동 - 1500점</ScoreText>
+            </Container>
+            <Container>
+                <MedalImg src="/images/2medal.svg"/>
+                <RankText>2위</RankText>
+                <ScoreText>우영우 - 1300점</ScoreText>
+            </Container>
+            <Container>
+                <MedalImg src="/images/3medal.svg"/>
+                <RankText>3위</RankText>
+                <ScoreText>구기준 - 1000점</ScoreText>
+            </Container>
+            </ContainerBox>
+        </Body>
     )
 }
 
