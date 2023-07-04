ff4d3f196f7acc5cf49317b6624afb8d2c3fdbc0
kimkh05 <nestjs01@naver.com>
2023-07-04 17:14:12 +0900

    header 버그

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../components/common/Header";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<>Test</>} /> 이런 형식으로 페이지 추가*/}
-        <Route path="/" element={<>Test</>} />
+        <Route path="/" element={<Header />} />
       </Routes>
     </BrowserRouter>
   );
