623a8d73b083a79567bf8f0451475c850ea3e5c5
kimkh05 <nestjs01@naver.com>
2023-07-04 13:16:21 +0900

    feat: components example

---
+++ src/components/test/index.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Test = () => {
+  return (
+    <Wrapper>
+      <span>jsx example</span>
+    </Wrapper>
+  );
+};
+
+export default Test;
+
+const Wrapper = styled.div`
+  // 여기 안에서 css 작성하면 돼
+`;
