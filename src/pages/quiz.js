f0475e303f54baa22059b8e0e889bd636bd422aa
kimkh05 <nestjs01@naver.com>
2023-07-04 19:56:18 +0900

    커서 호버 이미지

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,93 +1,87 @@
 import React from "react";
 import { styled } from "styled-components";
-import Header from "../components/common/Header"
+import Header from "../components/common/Header";
+
+const Quiz = () => {
+  return (
+    <Body>
+      <Header />
+      <Img src="/images/quizimg.svg"></Img>
+      <MainText>안전 퀴즈를 풀어보아요!</MainText>
+      <QuizBtn>퀴즈 도전하기</QuizBtn>
+      <LinkDiv>
+        <LinkSpanGray>순위가 궁금하세요?</LinkSpanGray>
+        <LinkSpanBlack href="">&nbsp;순위 확인</LinkSpanBlack>
+      </LinkDiv>
+    </Body>
+  );
+};
+
+export default Quiz;
 
 const Body = styled.div`
-display:flex;
-flex-direction:column;
-align-items:center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Img = styled.img`
-width: 450px;
-height: 450px;
-margin-top:123px;
+  width: 450px;
+  height: 450px;
+  margin-top: 123px;
 `;
 
 const MainText = styled.div`
-color: #000;
-font-size: 32px;
-font-family: Inter;
-font-style: normal;
-font-weight: 700;
-line-height: 32px;
-margin-top:37px;
+  color: #000;
+  font-size: 32px;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 32px;
+  margin-top: 37px;
 `;
 
 const QuizBtn = styled.button`
-margin-top:50px;
-display: inline-flex;
-padding: 0px 42px;
-justify-content: center;
-align-items: center;
-border-radius: 10px;
-background: #5AFF9C;
-`;
-
-const QuizBtnText = styled.span`
-color: #FFF;
-font-size: 20px;
-font-family: Noto Sans;
-font-style: normal;
-font-weight: 700;
-line-height: 39px;
+  margin-top: 50px;
+  display: inline-flex;
+  padding: 0px 42px;
+  justify-content: center;
+  cursor: pointer;
+  align-items: center;
+  border-radius: 10px;
+  background: #5aff9c;
+  color: #fff;
+  font-size: 20px;
+  font-family: Noto Sans;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 39px;
 `;
 
 const LinkDiv = styled.div`
-margin-top:27px;
-display:flex;
-justify-content:center;
+  margin-top: 27px;
+  display: flex;
+  justify-content: center;
 `;
 
 const LinkSpanGray = styled.span`
-color:#777777;
-font-size: 24px;
-font-family: Inter;
-font-style: normal;
-font-weight: 400;
-line-height: 32px;
+  color: #777777;
+  font-size: 24px;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px;
 `;
 
 const LinkSpanBlack = styled.a`
-color:#000000;
-font-size: 24px;
-font-family: Inter;
-font-style: normal;
-font-weight: 400;
-line-height: 32px;
+  color: #000000;
+  font-size: 24px;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+  }
 `;
-
-function Quiz() {
-    return (
-        <Body>
-            <Header/>
-            <Img src="/images/quizimg.svg"></Img>
-            <MainText>안전 퀴즈를 풀어보아요!</MainText>
-            <QuizBtn>
-                <QuizBtnText>
-                    퀴즈 도전하기
-                </QuizBtnText>
-            </QuizBtn>
-            <LinkDiv>
-                <LinkSpanGray>
-                    순위가 궁금하세요?
-                </LinkSpanGray>
-                <LinkSpanBlack href="">
-                    &nbsp;순위 확인
-                </LinkSpanBlack>
-            </LinkDiv>
-        </Body>
-    )
-}
-
-export default Quiz;
