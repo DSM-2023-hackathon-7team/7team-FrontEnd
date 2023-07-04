452367bd900a76831550dc1aec841e3b4e1a502d
kimkh05 <nestjs01@naver.com>
2023-07-04 20:37:42 +0900

    이미지 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 // export { default as 사용 이름 } from './경로';
 export { default as Clue } from "./image/clue.svg";
 export { default as Profile } from "./image/profile.svg";
+export { default as FindImage } from "./image/findImage.svg";
