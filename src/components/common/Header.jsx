dd43ba5821f663946429709043373184a572d56d
kimkh05 <nestjs01@naver.com>
2023-07-04 17:06:48 +0900

    헤더 퍼블리싱

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
+import { Profile } from "../../assets";
+
+const Header = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [name, setName] = useState("");
+
+  const onChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    setIsLogin(accessToken ? true : false);
+
+    if(isLogin) {
+      // 이름 요청
+    }
+  }, []);
+
+  return (
+    <_Wrapper>
+      <span>Logo</span>
+      <_RightWrapper>
+        <SearchBar onChange={onChange} value={searchValue} />
+        <_HeaderNav>안전 퀴즈</_HeaderNav>
+        <_HeaderNav>안전 사고</_HeaderNav>
+        <_HeaderNav>안전 뉴스</_HeaderNav>
+        <_HeaderNav>사고 게시판</_HeaderNav>
+        {isLogin ? (
+          <_ProfileWrapper>
+            <_Image src={Profile} alt="profile" />
+            <_Name>Test</_Name>
+          </_ProfileWrapper>
+        ) : (
+          <_LoginButton>로그인</_LoginButton>
+        )}
+      </_RightWrapper>
+    </_Wrapper>
+  );
+};
+
+export default Header;
+
+const _Wrapper = styled.header`
+  width: 100vw;
+  height: 80px;
+  display: flex;
+  padding: 13px 80px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const _RightWrapper = styled.div`
+  width: 60%;
+  display: flex;
+`;
+
+const _HeaderNav = styled(Link)`
+  font-size: 20px;
+  color: #000000;
+  font-weight: 400;
+  line-height: 32px;
+  text-decoration: none;
+  margin-right: 30px;
+  display: flex;
+  align-items: center;
+`;
+
+const _LoginButton = styled.button`
+  padding: 0 22px;
+  border-radius: 10px;
+  background: #5aff9c;
+  color: white;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 39px;
+`;
+
+const _Image = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+
+const _ProfileWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const _Name = styled.span`
+  color: #000;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px;
+  margin-left: 20px;
+  display: flex;
+  align-items: center;
+`;
---
+++ src/components/common/SearchBar.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { Clue } from "../../assets";
+
+const SearchBar = ({ onChange, value }) => {
+  return (
+    <_Wrapper>
+      <_Input type="text" placeholder="안전 정보를 입력해주세요." onChange={onChange} value={value} />
+      <img src={Clue} alt="clue" />
+    </_Wrapper>
+  );
+};
+
+export default SearchBar;
+
+const _Wrapper = styled.div`
+  width: 323px;
+  height: 39px;
+  display: flex;
+  padding: 5px 20px;
+  border-radius: 30px;
+  background-color: #f8f8f8;
+  justify-content: space-between;
+  align-items: center;
+  margin-right: 40px;
+  
+`;
+
+const _Input = styled.input`
+  width: 80%;
+  height: 100%;
+  ::placeholder {
+    color: #b4b0b0;
+  }
+`;
