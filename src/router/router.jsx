4a323bcbf499bfff9c3f04ba4fb06385d876550a
six-standard <dbrrl1127@gmail.com>
2023-07-04 21:47:37 +0900

    detail.js

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Create from "../pages/create";
+import Detail from "../pages/detail";
 import Quiz from "../pages/quiz";
 import Rank from "../pages/rank";
 import Quizgame from "../pages/quizgame";
@@ -11,7 +12,7 @@ export const Router = () => {
         {/* <Route path="/" element={<>Test</>} /> 이런 형식으로 페이지 추가*/}
         <Route path="/" element={<>Test</>} />
         <Route path="/create" element={<Create />} />
-        <Route path="/detail" element={<>Test</>} />
+        <Route path="/detail" element={<Detail />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/rank" element={<Rank />} />
         <Route path="/quizgame" element={<Quizgame />} />
