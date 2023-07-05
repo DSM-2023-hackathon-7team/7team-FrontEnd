5176c9973be4470bd25ffed4ba4d73b88a5e5daa
kimkh05 <nestjs01@naver.com>
2023-07-05 09:45:54 +0900

    퍼블리싱

--- src/components/common/list.jsx
+++ src/components/common/list.jsx
@@ -1,77 +1,67 @@
 import styled from "styled-components";
+import { Profile } from "../../assets";
+import { Heart } from "../../assets/image/svgs/Heart";
 
-const TipsList = ({hearts, title, data}) => {
-    return (
-        <Wrapper>
-            <Icon/>
-            <div>
-            <Texts>
-                <Title>{title}</Title>
-               
-            </Texts>
-<Text>
-<Data>{data}</Data>
-                <img src="/images/heart.svg" alt="Likes"></img>
-                <h1>{hearts}</h1>
-
-</Text>
-            </div>
-        </Wrapper>
-    )
-}
+const TipsList = ({ hearts, title, data }) => {
+  return (
+    <_Wrapper>
+      <_Image src={Profile} alt="post picture" />
+      <_SmallWrapper>
+        <_Title>{title}</_Title>
+        <_Items>
+          <span>{data}</span>
+          <_HeartWrapper>
+            <Heart />
+            <span>{hearts}</span>
+          </_HeartWrapper>
+        </_Items>
+      </_SmallWrapper>
+    </_Wrapper>
+  );
+};
 
 export default TipsList;
 
-const Wrapper = styled.div`
-    display: flex;
-    justify-items: center;
-    flex-direction: row; 
-    align-items: center;
-    width: 1180px;
-    height: 150px;
-    flex-shrink: 0;
-    background: #FDFDFD;
+const _Wrapper = styled.div`
+  width: 1180px;
+  height: 150px;
+  background-color: #fdfdfd;
+  padding: 15px 60px;
+  display: flex;
+  align-items: center;
 `;
 
-const Text = styled.div`
-    width: 100%;
-    display: flex;
-`
-
-const Texts = styled.div`
-    margin-left: 30px;
+const _Image = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-right: 40px;
 `;
 
-const Likes = styled.div`
-    display: inline-flex;
-    align-items: center;
-    margin-top: 90px;
-    gap: 5px;
+const _HeartWrapper = styled.div`
+  width: 5%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const Icon = styled.div`
-    display: block;
-    width: 120px;
-    height: 120px;
-    flex-shrink: 0;
-    border-radius: 5px;
-    background: url(<path-to-image>), lightgray 50% / cover no-repeat;
+const _Title = styled.h2`
+  color: #000;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 32px;
 `;
 
-const Title = styled.h1`
-    color: #000;
-    font-size: 24px;
-    font-family: Inter;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 32px;
+const _SmallWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 `;
 
-const Data = styled.h1`
-    color: #000;
-    font-size: 20px;
-    font-family: Inter;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 32px;
+const _Items = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
