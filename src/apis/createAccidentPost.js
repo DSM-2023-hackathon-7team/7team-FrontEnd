8088675de70d746e79d92a6bc9c9613996d3f61c
kimkh05 <nestjs01@naver.com>
2023-07-06 02:18:33 +0900

    비동기

--- src/apis/createAccidentPost.js
+++ src/apis/createAccidentPost.js
@@ -1,6 +1,6 @@
 import instance from "./axios";
 
-export const createAccidentPost = (data) => {
-  const response = instance.post("/accidents", data);
+export const createAccidentPost = async (data) => {
+  const response = await instance.post("/accidents", data);
   return response;
 };
