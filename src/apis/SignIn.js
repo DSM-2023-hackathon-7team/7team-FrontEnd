2d5a01b0954433c8dd37ada8152b0ae4f718e391
kimkh05 <nestjs01@naver.com>
2023-07-05 11:57:26 +0900

    prettier

--- src/apis/SignIn.js
+++ src/apis/SignIn.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const SignIn = (data) => {
-    const response = axios.post('/users/signin', data);
-    return response;
-}
+  const response = axios.post("/users/signin", data);
+  return response;
+};
